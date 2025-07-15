3d262743f041cd6a7ce9fab54f322afd51c0d831
rajesh-itgeeks <rajeshchoudhary@itgeeks.com>
2025-07-15 13:20:15 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix";
 
 installGlobals({ nativeFetch: true });
 
@@ -62,6 +63,7 @@ export default defineConfig({
         v3_routeConfig: true,
       },
     }),
+    vercelPreset(),
     tsconfigPaths(),
   ],
   build: {
